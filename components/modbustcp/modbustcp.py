3590cb4544555c075adacbb0ae102daea8292e01
Villersfr2 <152911529+Villersfr2@users.noreply.github.com>
2024-12-12 11:11:34 +0100

    Update modbustcp.py

--- components/modbustcp/modbustcp.py
+++ components/modbustcp/modbustcp.py
@@ -1,1 +1,15 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID
 
+modbustcp_ns = cg.esphome_ns.namespace("modbustcp")
+ModbusTCPSensor = modbustcp_ns.class_("ModbusTCPSensor", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ModbusTCPSensor),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
